0e87bfb895b858409abce474aa5bea7b6bad0528
Rajasegar <rajasegar.c@gmail.com>
2022-02-25 12:16:53 +0530

    chore: setting isLocal env var to true in webpack config

--- apps/root-config/webpack.config.js
+++ apps/root-config/webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = 'starwars';
+  webpackConfigEnv.isLocal = true;
   const defaultConfig = singleSpaDefaults({
     orgName,
     projectName: 'root-config',
@@ -15,6 +16,10 @@ module.exports = (webpackConfigEnv, argv) => {
   const finalConfig = merge(defaultConfig, {
     // const finalConfig = merge(nxconfig, {
     // modify the webpack config however you'd like to by adding to this object
+    devServer: {
+      port: 9000,
+      onListening: () => {},
+    },
     plugins: [
       new HtmlWebpackPlugin({
         inject: false,
@@ -27,6 +32,6 @@ module.exports = (webpackConfigEnv, argv) => {
     ],
   });
 
-  // console.log(finalConfig);
+  console.log(finalConfig);
   return finalConfig;
 };
